6bbc44f0a0a746e12e1276cc62c5f38bdd463b7e
Juan Andres Velez <rohenesj@uninorte.edu.co>
2024-03-19 14:01:27 -0500

    Changed title of index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GPS Tracker!</title>
+    <title>GPS Tracker Website</title>
 
     <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
     <link rel="stylesheet" href="custom.css">
