0ecbbbbbfc048754e525cd8ae177f90cb0e2f8ca
Carlos De La Rosa <caandeca@hotmail.com>
2024-04-01 03:34:49 +0000

    The in a timestamp range query, new index with a hyperlink to another website with the new route record feature was added

--- getcoordinates2.php
+++ getcoordinates2.php
@@ -3,35 +3,46 @@
 $cred = file_get_contents('credentials.json');
 $db = json_decode($cred);
 
-
 $pgsql_server = $db->pg_endpoint;
 $pgsql_user = $db->pg_user;
 $pgsql_password = $db->pg_password;
 $pgsql_db = $db->pg_db;
 
-try {
 
-    $pgsql_connection = new PDO("pgsql:host=$pgsql_server;dbname=$pgsql_db", $pgsql_user, $pgsql_password);
-    $pgsql_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    	// Get time range from user
+    $startTimestamp = $_POST['startTime'];
+    $endTimestamp = $_POST['endTime'];
+    //echo "Start Timestam2: " . $startTimestamp . "<br>";
+    //echo "End Timestamp2: " . $endTimestamp . "<br>";
 
-    $pgsql_query = "SELECT longitude, latitude FROM coordinates ORDER BY timestamp DESC LIMIT 1";
-    $pgsql_statement = $pgsql_connection->query($pgsql_query);
-    $rows = $pgsql_statement->fetchAll(PDO::FETCH_ASSOC);
+    try {
+        // PostgreSQL connection
+	    $conn = new PDO("pgsql:host=$pgsql_server;dbname=$pgsql_db", $pgsql_user, $pgsql_password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    if ($rows) {
-        // Inicializar un array para almacenar las características (features)
+        // Query in time range
+        $query = "SELECT longitude, latitude FROM coordinates WHERE timestamp >= :start AND timestamp <= :end";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':start', $startTimestamp, PDO::PARAM_INT);
+        $stmt->bindParam(':end', $endTimestamp, PDO::PARAM_INT);
+        $stmt->execute();
+
+        // Get filtered coordinates 
+        $coordenadas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        // Crear un objeto GeoJSON válido
         $geojson = array(
             'type' => 'FeatureCollection',
             'features' => array()
         );
 
-        // Iterar sobre las filas y crear un array de características GeoJSON
-        foreach ($rows as $row) {
+        foreach ($coordenadas as $coordenada) {
             $feature = array(
                 'type' => 'Feature',
                 'geometry' => array(
                     'type' => 'Point',
-                    'coordinates' => array(floatval($row['longitude']), floatval($row['latitude']))
+                    'coordinates' => array(floatval($coordenada['longitude']), floatval($coordenada['latitude']))
                 ),
                 'properties' => array(
                     // Puedes agregar más propiedades si lo deseas
@@ -40,21 +51,21 @@ try {
             array_push($geojson['features'], $feature);
         }
 
-
         // Salida del GeoJSON
         header('Content-type: application/json');
         echo json_encode($geojson);
 
+        //echo json_encode($coordenadas);
 
-    } else {
-        echo "No se encontraron coordenadas en la base de datos PostgreSQL.";
+        // Show obtnaied coordinates
+        //foreach ($coordenadas as $coordenada) {
+            //echo "<p>Longitude: " . $coordenada['longitude'] . ", Latitude: " . $coordenada['latitude'] . "</p>";
+       // }
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
     }
 
-} catch(PDOException $e) {
-    echo "Error: " . $e->getMessage();
-
-} finally {
-    $pgsql_connection = null;
+    	// Disconnect from Data Base
+    $conn = null;
 }
-
 ?>
--- index.php
+++ index.php
@@ -3,8 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GPS Tracker Website</title>
-
+    <title>GPS Tracker!</title>
+    <style>
+    header {
+      display: flex;
+      align-items: center;
+    }
+    h1 {margin-right: 100px;}
+    </style>
     <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
     <link rel="stylesheet" href="custom.css">
     <style>
@@ -18,7 +24,8 @@
 </head>
 <body>
   <header>
-    <h1>GPS Tracker Hola Clase </h1>
+    <h1>GPS Tracker</h1>
+    <a href="/Route_Records.php">Fetch Route</a>
   </header>
   <main>
     <div class="container">
@@ -29,132 +36,101 @@
             <p id="date">Date: </p>
             <p id="time">Time: </p>
             <p id="timestamp">Timestamp: </p>
-            <button type="button" id="gpsTrackerButton">GPS Tracker</button>
-	    <button type="button" id="fetchButton">Trace Route</button>
+            <button type="button" id="gpsTrackerButton">Erase Route</button>
         </div>
         <div class="item2">
             <div id="map"></div>
         </div>
     </div>
   </main>
-
+  
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
     <script>
-        var map = L.map('map').setView([10.983594, -74.804334], 15);
-        var marker = null;
+    var map = L.map('map').setView([10.983594, -74.804334], 15);
+    var marker = null;
+    var route = null; 
+    var lastCoordinate = null; 
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-        var APPicon = L.icon({
-            iconUrl: '/gpsmarker.png',
-            iconSize: [38, 38],
-            iconAnchor: [19, 38],
-            popupAnchor: [0, -38]
-        });
+    var APPicon = L.icon({
+        iconUrl: '/gpsmarker.png',
+        iconSize: [38, 38],
+        iconAnchor: [19, 38],
+        popupAnchor: [0, -38]
+    });
 
-        function updateMarker() {
-            $.ajax({
-                url: 'getcoordinates.php',
-                type: 'GET',
-                success: function(response){
-                    console.log(response)
-                    var data = JSON.parse(response);
-                    let hour = data.date.split(" ");
+    function updateMarker() {
+        $.ajax({
+            url: 'getcoordinates.php',
+            type: 'GET',
+            success: function(response){
+                console.log(response)
+                var data = JSON.parse(response);
+                let hour = data.date.split(" ");
 
-                    function convertToTimeZone(dateString) {
-                        var date = new Date(dateString);
-                        return date.toLocaleTimeString('en-US', { timeZone: timezone });
-                    }
+                function convertToTimeZone(dateString) {
+                    var date = new Date(dateString);
+                    return date.toLocaleTimeString('en-US', { timeZone: timezone });
+                }
 
-                    function convertDateToTimeZone(dateString) {
-                        var date = new Date(dateString);
-                        return date.toLocaleDateString('en-US', { timeZone: timezone });
-                    }
+                function convertDateToTimeZone(dateString) {
+                    var date = new Date(dateString);
+                    return date.toLocaleDateString('en-US', { timeZone: timezone });
+                }
 
-                    var timezone = document.querySelector('meta[name="timezone"]').getAttribute('content');
+                var timezone = document.querySelector('meta[name="timezone"]').getAttribute('content');
 
-                    $("#longitude").text("Longitude: " + data.longitude);
-                    $("#latitude").text("Latitude: " + data.latitude);
-                    $("#altitude").text("Altitude: " + data.altitude);
-                    $("#date").text("Date: " + convertDateToTimeZone(data.date));
-                    $("#time").text("Time: " + convertToTimeZone(data.date));
-                    $("#timestamp").text("Timestamp: " + data.timestamp);
-                    var latlng = [parseFloat(data.latitude), parseFloat(data.longitude)];
-                    if (marker === null) {
-                        marker = L.marker(latlng, { icon: APPicon }).addTo(map)
-                            .bindPopup('This is a default location. <br> Connecting to Data Base...')
-                            .openPopup();
-                    } else {
-                        marker.setLatLng(latlng);
-                        marker.bindPopup('Latitude: ' + data.latitude + '<br>Longitude: ' + data.longitude).openPopup();
-                    }
+                $("#longitude").text("Longitude: " + data.longitude);
+                $("#latitude").text("Latitude: " + data.latitude);
+                $("#altitude").text("Altitude: " + data.altitude);
+                $("#date").text("Date: " + convertDateToTimeZone(data.date));
+                $("#time").text("Time: " + convertToTimeZone(data.date));
+                $("#timestamp").text("Timestamp: " + data.timestamp);
+                var latlng = [parseFloat(data.latitude), parseFloat(data.longitude)];
+                if (marker === null) {
+                    marker = L.marker(latlng, { icon: APPicon }).addTo(map)
+                        .bindPopup('Connecting to Data Base...')
+                        .openPopup();
+                } else {
+                    marker.setLatLng(latlng);
+                    marker.bindPopup('Latitude: ' + data.latitude + '<br>Longitude: ' + data.longitude).openPopup();
+                }
+                map.setView(latlng);
+
+                if (JSON.stringify(latlng) !== JSON.stringify(lastCoordinate)) {
+                    drawRoute(latlng);
                     map.setView(latlng);
                 }
-            });
-        }
-
-        var route = null; 
-        var markerInterval = null;
-
-        $('#fetchButton').click(function() {
-            clearInterval(markerInterval);
-            var startTime = $('#startTime').val();
-            var endTime = $('#endTime').val();
-            var startTimestamp = Math.floor(new Date(startTime).getTime() / 1000);
-            var endTimestamp = Math.floor(new Date(endTime).getTime() / 1000);
-            
-            if (marker !== null) {
-                map.removeLayer(marker);
-                marker = null;
-            }
-            
-            function updatePosition(){
-                $.ajax({
-                    url: 'getcoordinates2.php',
-                    method: 'GET',
-                    success: function(response) {
-                        var coordinates = response.features[0].geometry.coordinates;
-
-                        var latLng = L.latLng(coordinates[1], coordinates[0]);
-                        if (marker === null) {
-                            marker = L.marker(latLng, { icon: APPicon }).addTo(map);
-                        } else {
-                            marker.setLatLng(latLng)
-                            marker.bindPopup('Latitude: ' + coordinates[1] + '<br>Longitude: ' + coordinates[0])
-                            .openPopup();
-                            
-                        }
-
-                        if (route === null) {
-                            route = L.polyline([latLng]).addTo(map);
-                        } else {
-                            route.addLatLng(latLng);
-                        }
-                        map.setView(latLng);
-            },
-            error: function(xhr, status, error) {
-                console.error(xhr.responseText);
-            }
-                });
-                
-            }
-
-            setInterval(updatePosition, 3000);
-
-        });
-
-        $('#gpsTrackerButton').click(function() {
-            updateMarker();
-            markerInterval = setInterval(updateMarker, 3000);
-            if (route !== null) {
-                        route.remove();
             }
         });
+    }
 
-    </script>
+    function drawRoute(newCoordinate) {
+
+    if (lastCoordinate !== null) {
+        route = L.polyline([lastCoordinate, newCoordinate], {color: 'blue'}).addTo(map);
+    }
+    lastCoordinate = newCoordinate;
+}
+
+    updateMarker();
+    setInterval(updateMarker, 3000);
+
+    $('#gpsTrackerButton').click(function() {
+        map.eachLayer(function(layer) {
+            if (layer instanceof L.Polyline) {
+                map.removeLayer(layer);
+            }
+        });
+    });
+    
+</script>
+
+
 </body>
 </html>
 
