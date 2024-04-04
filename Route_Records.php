f3525ecba08cf05ed9f544a3faf6f9862b73ed41
Juan Andres Velez <rohenesj@uninorte.edu.co>
2024-04-04 17:00:11 -0500

    Fixed import mistake

--- Route_Records.php
+++ Route_Records.php
@@ -72,7 +72,7 @@
     </div>
 
     <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
-    <script src="src/Route_Records_test.js"></script>
+    <script src="src/Route_Records.js"></script>
     <!--<script src="src/maxTime.js"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" 
     integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
