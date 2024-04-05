04fe694e08a28fce81b30cd668fb25349cc299c8
Juan Andres Velez <rohenesj@uninorte.edu.co>
2024-04-04 19:37:04 -0500

    Button disabled when no dates

--- Route_Records.php
+++ Route_Records.php
@@ -60,7 +60,7 @@
                 <h2>Track vehicle in a given time:</h2>
                 <form id="searchForm" method="post" action="getcoordinates2.php">
                     <input type="text" name="datetimes" />
-                    <button type="button" id="fetchButton" class="btn btn-outline-primary mt-3">Fetch Route</button>
+                    <button disabled type="button" id="fetchButton" class="btn btn-outline-primary mt-3">Fetch Route</button>
                 </form>
                 <div id="timestamps"></div>
                 <div id="Error"></div>
