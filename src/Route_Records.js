aff9411bfc1d4a4fe10273e392a32ff1d068e3be
Juan Andres Velez <rohenesj@uninorte.edu.co>
2024-04-10 19:40:27 -0500

    Created experimental Route_Records.js file

--- src/Route_Records.js
+++ src/Route_Records.js
@@ -121,154 +121,8 @@ $('#fetchButton').click(function() {
     });
 });
 
-
-
-    control.on('markgeocode', function(e) {
-        var location = e.geocode.center;
-        latitude = location.lat;
-        longitude = location.lng;
-        removeMarkers();
-    
-        if (selectMarker === null) {
-            selectMarker = L.marker([latitude, longitude], { icon: APPicon }).addTo(map)
-                .bindPopup('Latitude: ' + latitude + '<br>Longitude: ' + longitude)
-                .openPopup();
-            map.setView([latitude, longitude], 16);
-        }
-
-        console.log("Las coordenadas de la ubicación son: Latitud =", latitude, ", Longitud =", longitude);
-        latRange = latitude;
-        longRange = longitude;
-
-        leftCorner = [latRange - 0.00225, longRange - 0.00225];
-        rightCorner = [latRange + 0.00225, longRange + 0.00225];
-        var bounds = [leftCorner, rightCorner]
-
-        map.on('click', onlyaddMarker);
-
-        L.rectangle(bounds, {
-            color: "blue", 
-            fillColor:"blue",
-            fillOpacity: 0.2
-        }).addTo(map);
-        map.fitBounds(bounds);
-
-        $.ajax({
-        url: 'getcoordinates3.php',
-        method: 'POST',
-        data: {
-            startTime: startTimestamp,
-            endTime: endTimestamp
-        },
-        success: function(response) {
-            $('#timestamps').html("<p>Start Timestamp: " + startTimestamp + "</p><p>End Timestamp: " + endTimestamp + "</p>");
-            $('#Error').empty();
-            var coordinates = response;
-            console.log(coordinates);
-            if (!coordinates || coordinates.features.length === 0) {
-                map.setView([10.983594, -74.804334], 15)
-                $('#Error').html("<p class='error-message'>No coordinates in the selected time range.</p>");
-                Errormarker = L.marker([10.983594, -74.804334], { icon: APPicon }).addTo(map)
-                .bindPopup('No coordinates in the selected time range')
-                .openPopup();
-                
-            return; 
-            }
-            latLngs = []
-            map.eachLayer(function(layer) {
-                if (layer instanceof L.Marker) {
-                map.removeLayer(layer);
-                }
-            });
-            coordinates.features.forEach(function(feature, index) {
-                var coords = feature.geometry.coordinates;
-                var tstamp = parseFloat(feature.properties.timestamp);
-                var date = feature.properties.date;
-                console.log(coords);
-                if ((coords[0] >= (longRange - 0.00225)) && (coords[0] <= (longRange + 0.00225))) {
-                    console.log("In Longitud Range",coords[0]);
-                    if ((coords[1] >= (latRange - 0.00225)) && (coords[1] <= (latRange + 0.00225))) {
-                        console.log("In Latitud Range",coords[1]);
-                        var latLng = L.latLng(coords[1], coords[0]);
-                        latLngs.push(latLng);
-                        var marker = L.marker([coords[1],coords[0]]).addTo(map)
-                        .bindPopup('Marked at ' + date);
-                    }
-                }
-            });
-
-
-        }
-    });
-});
-
-
-
-    let isSearchEnabled = true;
-
-    document.getElementById('toggleButton').addEventListener('click', function() {
-        if (isSearchEnabled) {
-
-            removeMarkers();
-            map.eachLayer(function(layer) {
-                if (layer instanceof L.Marker) {
-                    map.removeLayer(layer);
-                }
-            });            
-            map.on('click', addMarker);
-            map.removeControl(control);
-            this.textContent = 'Type place';
-        } else {
-
-            removeMarkers();
-            map.eachLayer(function(layer) {
-                if (layer instanceof L.Marker) {
-                    map.removeLayer(layer);
-                }
-            });
-            map.addControl(control);
-            map.on('click', addMarker); 
-            map.off('click');
-            this.textContent = 'Select on map';
-
-            map.setView([10.983594, -74.804334], 15);
-
-            seed = L.marker([10.991865,  -74.833637], { icon: APPicon }).addTo(map)
-            .bindPopup('Select time range <br>' + 'and click the <br>' + 'Fetch Route Button.')
-            .openPopup();
-
-            seed2 = L.marker([10.991865,  -74.773420], { icon: APPicon }).addTo(map)
-                    .bindPopup('To search a place, click <br>' + 'on the search button. <br>' + 'Or you can click on <br>' + 'Select on map.')
-                    .openPopup();
-                                        
-        }
-        isSearchEnabled = !isSearchEnabled;
-    });
-
-
-    function addMarker(e) {
-        var latitude = e.latlng.lat;
-        var longitude = e.latlng.lng;
-        console.log("Las coordenadas de la ubicación son: Latitud =", latitude, ", Longitud =", longitude);
-        removeMarkers();
-
-        latRange = latitude;
-        longRange = longitude;
-
-        leftCorner = [latRange - 0.00225, longRange - 0.00225];
-        rightCorner = [latRange + 0.00225, longRange + 0.00225];
-        var bounds = [leftCorner, rightCorner]
-
-        L.rectangle(bounds, {
-            color: "blue", 
-            fillColor:"blue",
-            fillOpacity: 0.2
-        }).addTo(map);
-        map.fitBounds(bounds);
-
-        
-
-        $.ajax({
+function fetchCoordinates(startTimestamp,endTimestamp,latRange,longRange) {
+    $.ajax({
         url: 'getcoordinates3.php',
         method: 'POST',
         data: {
@@ -315,70 +169,59 @@ $('#fetchButton').click(function() {
 
         }
     });
+}
 
+control.on('markgeocode', function(e) {
+    var location = e.geocode.center;
+    latitude = location.lat;
+    longitude = location.lng;
+    removeMarkers();
+
+    if (selectMarker === null) {
+        selectMarker = L.marker([latitude, longitude], { icon: APPicon }).addTo(map)
+            .bindPopup('Latitude: ' + latitude + '<br>Longitude: ' + longitude)
+            .openPopup();
+        map.setView([latitude, longitude], 16);
     }
 
-        function removeMarkers() {
+    console.log("Las coordenadas de la ubicación son: Latitud =", latitude, ", Longitud =", longitude);
+    latRange = latitude;
+    longRange = longitude;
 
-            if (seed !== null) {
-                map.removeLayer(seed);
-            }
-    
-            if (seed2 !== null) {
-                map.removeLayer(seed2);
-            }
-            
-            if (Startmarker !== null) {
-                map.removeLayer(Startmarker);
-            }
-    
-            if (Endmarker !== null) {
-                map.removeLayer(Endmarker);
-            }
-    
-            if (Errormarker !== null) {
-                map.removeLayer(Errormarker);
-            }
-    
-            if (selectMarker !== null) {
-                map.removeLayer(selectMarker);
-                selectMarker = null;
-            }
-    
-            if (circle !== null) {
-                map.removeLayer(circle);
-                circle = null;
-            }
-    
-            map.eachLayer(function(layer) {
-                if (layer instanceof L.Polyline) {
-                    map.removeLayer(layer);
-                }
-            });
+    leftCorner = [latRange - 0.00225, longRange - 0.00225];
+    rightCorner = [latRange + 0.00225, longRange + 0.00225];
+    var bounds = [leftCorner, rightCorner];
 
-            map.eachLayer(function(layer) {
-                if (layer instanceof L.marker) {
-                    map.removeLayer(timeMarker)
-                }
-            });
-        }
-        function onlyAddMarker(e) {
-            var latitude = e.latlng.lat;
-            var longitude = e.latlng.lng;
-            console.log("Las coordenadas de la ubicación son: Latitud =", latitude, ", Longitud =", longitude);
-            removeMarkers();
-    
-            latRange = latitude;
-            longRange = longitude;
-    
-            leftCorner = [latRange - 0.00225, longRange - 0.00225];
-            rightCorner = [latRange + 0.00225, longRange + 0.00225];
-            var bounds = [leftCorner, rightCorner]
-    
-            L.rectangle(bounds, {
-                color: "blue", 
-                fillColor:"blue",
-                fillOpacity: 0.2
-            }).addTo(map);
-            map.fitBounds(bounds);
-        }
+    L.rectangle(bounds, {
+        color: "blue", 
+        fillColor:"blue",
+        fillOpacity: 0.2
+    }).addTo(map);
+    map.fitBounds(bounds);
+    fetchCoordinates(startTimestamp,endTimestamp,latRange,longRange);
+});
+
+function addMarker(e) {
+    var latitude = e.latlng.lat;
+    var longitude = e.latlng.lng;
+    console.log("Las coordenadas de la ubicación son: Latitud =", latitude, ", Longitud =", longitude);
+    removeMarkers();
+
+    latRange = latitude;
+    longRange = longitude;
+
+    leftCorner = [latRange - 0.00225, longRange - 0.00225];
+    rightCorner = [latRange + 0.00225, longRange + 0.00225];
+    var bounds = [leftCorner, rightCorner]
+
+    L.rectangle(bounds, {
+        color: "blue", 
+        fillColor:"blue",
+        fillOpacity: 0.2
+    }).addTo(map);
+    map.fitBounds(bounds);
+    fetchCoordinates(startTimestamp,endTimestamp,latRange,longRange);
+}
+
+
+map.on('click',addMarker);
