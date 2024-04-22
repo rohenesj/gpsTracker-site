32cefe9ef21efb502893dee9a4846da0d4ffb378
Juan Andres Velez <rohenesj@uninorte.edu.co>
2024-04-22 13:26:03 -0500

    Mobile design test 1

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,9 +1,17 @@
 function openNav() {
+  const screenWidth = window.screen.width;
+  if (screenWidth > 767){
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+  } else {
+    let size = screenWidth.toString() + 'px';
+    console.log(size);
+    document.getElementById("mySidebar").style.width = size;
+    document.getElementById("main").style.marginLeft = size;
   }
-  
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  }
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
