968b4052bfef69468f57056aed7bc8d0c2eb6525
Juan Andres Velez <rohenesj@uninorte.edu.co>
2024-05-06 20:22:49 -0500

    Reverted rework, used previous file as base

--- getcoordinates.php
+++ getcoordinates.php
@@ -3,11 +3,11 @@
 $cred = file_get_contents('credentials.json');
 $db = json_decode($cred);
 
+
 $pgsql_server = $db->pg_endpoint;
 $pgsql_user = $db->pg_user;
 $pgsql_password = $db->pg_password;
 $pgsql_db = $db->pg_db;
-
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -18,24 +18,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $table = "coordinates";
     }
 
-
     try {
- 
-	    $conn = new PDO("pgsql:host=$pgsql_server;dbname=$pgsql_db", $pgsql_user, $pgsql_password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
- 
-        $query = "SELECT longitude, latitude, altitude, date, timestamp, car_data FROM :truck ORDER BY timestamp DESC LIMIT 1";
-        $stmt = $conn->prepare($query);
-        $stmt->bindParam(':truck', $table, PDO::PARAM_INT);
-        $stmt->execute();
+        $pgsql_connection = new PDO("pgsql:host=$pgsql_server;dbname=$pgsql_db", $pgsql_user, $pgsql_password);
+        $pgsql_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $coordenadas = $stmt->fetch(PDO::FETCH_ASSOC);
-        echo json_encode($coordenadas);
+        $pgsql_query = "SELECT longitude, latitude, altitude, date, timestamp, car_data FROM $table ORDER BY timestamp DESC LIMIT 1";
+        $pgsql_statement = $pgsql_connection->query($pgsql_query);
+        $row = $pgsql_statement->fetch(PDO::FETCH_ASSOC);
+
+        if ($row) {
+            echo $truck;
+            //echo json_encode($row);
+        } else {
+            echo "No se encontraron coordenadas en la base de datos PostgreSQL.";
+        }
 
     } catch(PDOException $e) {
         echo "Error: " . $e->getMessage();
+
+    } finally {
+        $pgsql_connection = null;
     }
-    $conn = null;
 }
 ?>
