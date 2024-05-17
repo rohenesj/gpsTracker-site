e070af7aabba107a1593b05ae150f6db59dd0b1a
Juan Andres Velez <rohenesj@uninorte.edu.co>
2024-05-16 21:05:02 -0500

    Added fitBounds

--- src/Route_Records2.js
+++ src/Route_Records2.js
@@ -26,7 +26,7 @@ var truck1Filtered = null;
 var truck2Filtered = null;
 var truckFiltered = null;
 var focusedCoords = null;
-
+var group = null;
 
 function clearCoordinates() {
     windowCoords1 = [];
@@ -269,6 +269,7 @@ function selectPolyline() {
             smoothFactor: '2',
             weight: '5' 
         }).addTo(map);
+        group = new L.featureGroup([polylineData1,polylineData2]);
     }
     if (truckMode == "1") {
         focusedCoords = windowCoords1;
@@ -279,6 +280,7 @@ function selectPolyline() {
         $('#windowSlider1').empty();
         $('#windowSliderLabel1').html("<label for=\"myRange1\" class=\"form-label\">Timeline</label>");
         var slider = $('<input type="range" class="form-range "id="myRange1" value="0" min="0" max="' + maxValue + '" value="50">');
+        map.fitBounds(polylineLayer1.getBounds());
         $('#windowSlider1').append(slider);
     } else if (truckMode == "2") {
         focusedCoords = windowCoords2;
@@ -290,6 +292,7 @@ function selectPolyline() {
         $('#windowSliderLabel1').html("<label for=\"myRange1\" class=\"form-label\">Timeline</label>");
         var slider = $('<input type="range" class="form-range "id="myRange1" value="0" min="0" max="' + maxValue + '" value="50">');
         $('#windowSlider1').append(slider);
+        map.fitBounds(polylineLayer2.getBounds());
     } else {
         focusedCoords = bothTrucks;
         let maxValue = bothTrucks.length - 1;
@@ -302,6 +305,7 @@ function selectPolyline() {
         $('#windowSliderLabel1').html("<label for=\"myRange1\" class=\"form-label\">Timeline</label>");
         var slider = $('<input type="range" class="form-range "id="myRange1" value="0" min="0" max="' + maxValue + '" value="50">');
         $('#windowSlider1').append(slider);
+        map.fitBounds(group.getBounds());
     }
 }
 
@@ -402,7 +406,11 @@ $(document).ready(function() {
     $('#windowSlider1').on('input', '#myRange1', function() {
         var sliderValue = $(this).val();
         console.log(sliderValue);
-        removeMarkers();
+        map.eachLayer(function(layer) {
+            if (layer instanceof L.Marker) {
+            map.removeLayer(layer);
+            }
+        });
         var marker = L.marker([focusedCoords[sliderValue][0],focusedCoords[sliderValue][1]],{ icon: APPicon }).addTo(map)
         .bindPopup('Marked at ' + timeMessage(focusedCoords[sliderValue][2]) + '<br>RPM: ' + focusedCoords[sliderValue][3])
         .openPopup();
